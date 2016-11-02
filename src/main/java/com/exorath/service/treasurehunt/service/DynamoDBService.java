855383f6f60ed19b90913d703533f8694f2ccd6d
Yannick <yannickjeras@hotmail.com>
2016-11-02 09:50:11 +0000

    Stub implementation
    Quick implementation taken from
    http://docs.aws.amazon.com/amazondynamodb/latest/gettingstartedguide/GettingStarted.Java.03.html.
    Have yet to test it (put probably only works for new entries, need to
    check if exists and use update instead).

--- src/main/java/com/exorath/service/treasurehunt/service/DynamoDBService.java
+++ src/main/java/com/exorath/service/treasurehunt/service/DynamoDBService.java
@@ -16,20 +16,60 @@
 
 package com.exorath.service.treasurehunt.service;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 import com.exorath.service.treasurehunt.Result;
 import com.exorath.service.treasurehunt.Treasure;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
 
 public class DynamoDBService extends SimpleService {
+	private static final Logger logger = new LoggerFactory.getLogger(DynamoDBService.class);
+
+	private final AmazonDynamoDBClient client;
+	private final DynamoDB db;
+	private final Table table;
+
+	public DynamoDBService() {
+		client = new AmazonDynamoDBClient().withEndpoint("http://localhost:8000");
+		db = new DynamoDB(client);
+		table = db.getTable("Treasures");
+	}
 
 	@Override
 	public Treasure[] getTreasures(UUID playerId) {
-		return new Treasure[0];
+		GetItemSpec spec = new GetItemSpec().withPrimaryKey("playerId", playerId);
+		Item outcome;
+		try {
+			outcome = table.getItem(spec);
+			System.out.println("GetItem succeeded: " + outcome);
+		} catch (Exception ex) {
+			System.err.println("getTreasures(" + playerId + ") failed:\n" + ex.getMessage());
+			return new Treasure[0];
+		}
+		Treasure[] treasures = new Treasure[outcome.numberOfAttributes()];
+		int i = 0;
+		for (Object o : outcome.asMap().values()) {
+			treasures[i++] = new Treasure(o.toString());
+		}
+		return treasures;
 	}
 
 	@Override
 	public Result setTreasure(UUID playerId, String treasureId) {
-		return new Result(false);
+		try {
+			PutItemOutcome outcome = table.putItem(new Item().withPrimaryKey("playerId", playerId, "treasureId", treasureId));
+			logger.info("setTreasure(" + playerId + ", " + treasureId + ") succeeded:\n" + outcome.getPutItemResult());
+		} catch (Exception ex) {
+			logger.error("setTreasure(" + playerId + ", " + treasureId + ") failed:\n" + ex.getMessage());
+			return new Result(false);
+		}
+		return new Result(true);
 	}
 }
